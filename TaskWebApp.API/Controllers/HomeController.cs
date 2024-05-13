c852721569adc94e279462d35a56c9d7dea61ac1
safakcivelek <safakcivelek3@gmail.com>
2024-05-13 15:12:14 +0300

    Added new project

---
+++ TaskWebApp.API/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskWebApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> GetContentAsync()
+        {
+            var myTask = new HttpClient().GetStringAsync("https://www.google.com");
+
+            var data = await myTask;
+
+            return Ok(data);
+        }
+    } 
+}
